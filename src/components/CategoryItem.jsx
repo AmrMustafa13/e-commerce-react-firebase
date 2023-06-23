70f07e44b987d8892aa8dce0ad3e4411c04fefb5
Amr Mustafa <amrmostafa1311@gmail.com>
2023-06-24 02:04:29 +0300

    fixed category shop now btn link

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const CategoryItem = ({ category }) => {
   return (
@@ -63,7 +64,7 @@ const CategoryItem = ({ category }) => {
             cursor-pointer
       "
       >
-        Shop Now
+        <Link to="/shop">Shop Now</Link>
       </span>
     </div>
   );
