04fd9d170448afc0d0d3e5f7ba245b87b1794331
Amr Mustafa <amrmostafa1311@gmail.com>
2023-06-25 16:06:37 +0300

    removed women section

--- src/components/CategoriesGrid.jsx
+++ src/components/CategoriesGrid.jsx
@@ -19,11 +19,6 @@ const categories = [
   },
   {
     id: 4,
-    title: "womens",
-    imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-  },
-  {
-    id: 5,
     title: "mens",
     imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
   },
